185569e1d4e63de2e27288b414e97514664cf02d
mithil <mithilsalunkhe@outlook.com>
2025-10-17 20:44:28 -0500

    create generate_embeds.py

---
+++ src/data/deduplication/generate_embeds.py
@@ -0,0 +1,6 @@
+from datasets import load_dataset
+from embed import embed_documents,load_model
+ds = load_dataset("Mithilss/nasdaq-external-data-processed")['train']
+documents = ds['Article']
+tokenizer, model  = load_model()
+embed_documents(documents=documents,model=model,tokenizer=tokenizer)

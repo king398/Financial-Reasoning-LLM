913ea6533322ccdb182c8b716aa353d8808998f2
mithil <mithilsalunkhe@outlook.com>
2025-10-23 20:18:16 -0500

    update gpu embedding

--- src/data/deduplication/generate_embeds.py
+++ src/data/deduplication/generate_embeds.py
@@ -3,7 +3,7 @@ from embed import embed_documents,load_model
 import numpy as np
 ds = load_dataset("Mithilss/nasdaq-external-data-2018-onwards")['train']
 documents = ds['Article']
-document_ids = ds['Date']
+document_ids = ds['uuid']
 tokenizer, model  = load_model()
 embedding = embed_documents(documents=documents,model=model,tokenizer=tokenizer,batch_size=64).numpy()
 np.savez("embeddings.npz", embedding=embedding, document_ids=document_ids)

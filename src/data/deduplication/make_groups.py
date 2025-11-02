0de858f1ee036b4cbe1f2ddea1c39c91beb4fba8
mithil <mithilsalunkhe@outlook.com>
2025-11-02 11:49:42 -0600

    dedup dataset

--- src/data/deduplication/make_groups.py
+++ src/data/deduplication/make_groups.py
@@ -2,7 +2,7 @@ import json
 from datasets import load_dataset
 with open("dedup_groups.json", "r") as f:
     data = json.load(f)
-
+print(data[0])
 keep_id = [x[0] for x in data]
 dataset = load_dataset("Mithilss/nasdaq-external-data-2018-onwards")
 print(keep_id[0])

bbedd9c5603cda877e789369c7f973cf9af1511b
mithil <mithilsalunkhe@outlook.com>
2025-11-02 11:46:39 -0600

    dedup dataset

--- src/data/deduplication/make_groups.py
+++ src/data/deduplication/make_groups.py
@@ -1,6 +1,10 @@
 import json
-
+from datasets import load_dataset
 with open("dedup_groups.json", "r") as f:
     data = json.load(f)
 
-    
+keep_id = [x[0] for x in data]
+dataset = load_dataset("Mithilss/nasdaq-external-data-2018-onwards")
+
+filter_dataset = dataset["train"].filter(lambda x: x["uuid"]  in keep_id,num_proc=32)
+filter_dataset.push_to_hub("Mithilss/nasdaq-external-data-2018-onwards-dedup")

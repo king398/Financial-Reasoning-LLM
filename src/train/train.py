5e0220871023d1cd35841ddacc29741c8520899f
mithil <mithilsalunkhe@outlook.com>
2025-11-09 20:00:45 -0600

    add training file

---
+++ src/train/train.py
@@ -0,0 +1,57 @@
+from unsloth import FastLanguageModel
+import torch
+from trl import SFTTrainer, SFTConfig
+from datasets import load_dataset
+from unsloth.chat_templates import train_on_responses_only
+train_dataset = load_dataset("Mithilss/financial-training")['train']
+validation_dataset = load_dataset("Mithilss/financial-validation")['validation']
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/Qwen3-4B-Instruct-2507",
+    max_seq_length = 16384, # Choose any for long context!
+    load_in_4bit = True,  # 4 bit quantization to reduce memory
+    load_in_8bit = False, # [NEW!] A bit more accurate, uses 2x memory
+    full_finetuning = False, # [NEW!] We have full finetuning now!
+    # token = "hf_...", # use one if using gated models
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 32, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 32,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = train_dataset,
+    eval_dataset = validation_dataset, # Can set up evaluation!
+    args = SFTConfig(
+        dataset_text_field = "text",
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 8, # Use GA to mimic batch size!
+        warmup_steps = 5,
+        num_train_epochs = 1, # Set this for 1 full training run.
+        learning_rate = 2e-5, # Reduce to 2e-5 for long training runs
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.001,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        report_to = "wandb", # Use TrackIO/WandB etc
+    ),
+)
+trainer = train_on_responses_only(
+    trainer,
+    instruction_part = "<|im_start|>user\n",
+    response_part = "<|im_start|>assistant\n",
+)
+
+trainer_stats = trainer.train()
